b61aa31cbf00bc15f3dc2dfee8b792993126a972
Aleksey Chipiga <aleksey.chipiga@abz.agency>
2017-10-12 00:53:35 +0300

    update project files

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
-var server = restify.createServer();
+var server = restify.createServer({
+    url: 'http://lexa-bot.herokuapp.com'
+});
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
