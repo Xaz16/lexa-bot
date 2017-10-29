616c0d97cd829054edd3e534f19976aa9f4c0f08
Aleksey Chipiga <aleksey.chipiga@abz.agency>
2017-10-29 03:32:22 +0300

    Check CI

--- tests/parseFile.spec.js
+++ tests/parseFile.spec.js
@@ -2,12 +2,12 @@ let chai = require('chai');
 let expect = chai.expect;
 let parseFile = require('../utils/parseFile');
 
-describe('ParseFile function', function() {
-    it('ParseFile() should return object or array from json which path provided in parameter', function() {
+describe('ParseFile function', function () {
+    it('ParseFile() should return object or array from json which path provided in parameter', function () {
 
         expect(parseFile('tests/data/parseFileExample.json')).to.have.all.keys('test', 'parse');
     });
-    it('If parseFile didn\'t find any files it should return empty object', function() {
+    it('If parseFile didn\'t find any files it should return empty object', function () {
 
         expect(parseFile('tests/data/parseFileExampleError.json')).to.be.an('object').that.is.empty;
     });
