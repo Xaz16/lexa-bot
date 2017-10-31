b6255ad9ae08f2a42bbadf5cf80f1e9abf0775a3
Aleksey Chipiga <aleksey.chipiga@abz.agency>
2017-11-01 01:03:38 +0300

    Added cards from devlife

--- utils/sendCard.js
+++ utils/sendCard.js
@@ -1,18 +1,15 @@
 const builder = require('botbuilder');
-const getCard = require('../models/getCard');
 
 function sendCard(address, card, bot) {
     let msg = new builder.Message().address(address);
         console.log(card);
         msg.addAttachment(
-            new builder.AnimationCard({
-                autostart: true,
-                image: card.image,
-                media: card.image,
-                text: card.text + '<br/> Rating: ' + card.rating,
-
-            })
+            new builder.AnimationCard()
+                .autostart(true)
+                .media(card.image)
+                .text(card.text + ' <br/>Rating:' + card.rating)
         );
+        msg.text('');
         bot.send(msg);
 }
 
