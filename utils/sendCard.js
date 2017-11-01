a45ea2e947168443a74af4ef30f57281682e70f7
Aleksey Chipiga <aleksey.chipiga@abz.agency>
2017-11-01 20:10:24 +0300

    test cards

--- utils/sendCard.js
+++ utils/sendCard.js
@@ -4,7 +4,7 @@ function sendCard(address, card, bot, session) {
     let msg = new builder.Message(session).address(address);
         console.log(card);
         msg.addAttachment(
-            new builder.VideoCard(session)
+            new builder.MediaCard(session)
                 .autostart(true)
                 .media([{url:'https:' + card.image}])
                 .text(card.text)
