411c5360c56488ae9f748b98a8b86a7370a6c458
Aleksey Chipiga <aleksey.chipiga@abz.agency>
2017-11-09 09:56:11 +0300

    Fix animation card

--- utils/sendCard.js
+++ utils/sendCard.js
@@ -4,9 +4,7 @@ function sendCard(address, card, bot, session) {
     let msg = new builder.Message(session).address(address);
         msg.addAttachment(
             new builder.AnimationCard(session)
-                .autostart(true)
-                .autoloop(true)
-                .media([{url:card.gifUrl}])
+                .media([{url:card.gifurl}])
                 .subtitle(card.text)
         );
         bot.send(msg);
